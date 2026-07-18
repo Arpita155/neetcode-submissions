dc9b7ac94f24b9b3fea52a15833e5f993b99a30a
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-07-18 12:56:56 +0530

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        int[] original = heights.clone();
+        Arrays.sort(heights);
+        int count = 0;
+
+        for(int i=0;i<heights.length;i++){
+            if(heights[i] != original[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
