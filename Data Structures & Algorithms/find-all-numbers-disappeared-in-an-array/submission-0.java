8791aa7c805333e5c2999244c8526c84f5eac771
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-09-16 09:53:18 +0530

    Add: find-all-numbers-disappeared-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i : nums){
+            hs.add(i);
+        }
+
+        for(int i=1;i<=nums.length;i++){
+            if(!hs.contains(i)){
+                res.add(i);
+            }
+        }
+
+        return res;
+    }
+}
